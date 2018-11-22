a5c1716813a6b381258bc5eb78fd6ba349d319e8
Brian Ko <brian.kostar@gmail.com>
2018-11-21 23:59:06 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const Index = () => <h2>Home</h2>;
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <BrowserRouter>
         <div>
           <h2>Libraies:</h2>
 
           <Link to="/react-router">React Router</Link>
 
-          <Route path="/react-router" children={() => Index}></Route>
+          <Route path="/react-router" component="ReactRouterShowcase"></Route>
         </div>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
