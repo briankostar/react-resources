ab90dd3471dbf76c8ad32f43a616af3dfe97ea2c
Brian Ko <brian.kostar@gmail.com>
2018-12-09 22:31:50 -0500

    update view

--- src/lib/react-router/ReactRouterShowcase.js
+++ src/lib/react-router/ReactRouterShowcase.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Menu, Header } from 'semantic-ui-react'
 
 import { DynamicSubRoute } from './DynamicSubRoute'
 import { SimpleLogin } from './SimpleLogin'
@@ -9,22 +10,39 @@ import { FourOhFour } from './404'
 import MarkdownViewer from '../../components/markdownViewer/markdownViewer'
 
 export class ReactRouterShowcase extends Component {
+    state = { activeItem: '' }
+
+    handleItemClick = (e, { name }) => {
+        this.setState({ activeItem: name });
+        this.props.history.push(`${this.props.match.url}${name}`)
+    }
 
     render() {
+        const { activeItem } = this.state
+
         return (
-            <div>ReactRouterShowcase
-                <Link to={`${this.props.match.url}/dynamic-sub-route`} >dynamic-sub-route</Link>
-                <Link to={`${this.props.match.url}/simple-login`} >Simple login</Link>
-                <Link to={`${this.props.match.url}/prompt`} >Prompt</Link>
-                <Link to={`${this.props.match.url}/404`} >FourOhFour</Link>
+            <div>
+                <Header as='h1'>React Router Showcase</Header>
+
+                <Menu tabular>
+                    <Menu.Item name='' active={activeItem === 'Notes'} onClick={this.handleItemClick} >Notes</Menu.Item>
+                    <Menu.Item name='/dynamic-sub-route' active={activeItem === 'Dynamic Sub Route'} onClick={this.handleItemClick} />
+                    <Menu.Item name='/simple-login' active={activeItem === 'Simple Login'} onClick={this.handleItemClick} />
+                    <Menu.Item name='/prompt' active={activeItem === 'Prompt'} onClick={this.handleItemClick} />
+                    <Menu.Item name='/404' active={activeItem === '404'} onClick={this.handleItemClick} />
+                </Menu>
 
                 <Route path={`${this.props.match.url}/dynamic-sub-route`} component={DynamicSubRoute}></Route>
                 <Route path={`${this.props.match.url}/simple-login`} component={SimpleLogin}></Route>
                 <Route path={`${this.props.match.url}/prompt`} component={PromptForm}></Route>
                 <Route path={`${this.props.match.url}/404`} component={FourOhFour}></Route>
 
-                <MarkdownViewer src='https://raw.githubusercontent.com/briankostar/react-resources/master/public/notes/react-router.md'></MarkdownViewer>
-            </div>
+                {
+                    this.props.location.pathname === '/react-router' ?
+                        <MarkdownViewer src='https://raw.githubusercontent.com/briankostar/react-resources/master/public/notes/react-router.md'></MarkdownViewer>
+                        : null
+                }
+            </div >
 
         )
     }
