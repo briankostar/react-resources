7fd0ccbfe94be9a5b77d935054f9992ea9535221
Brian Ko <brian.kostar@gmail.com>
2018-12-14 22:50:48 -0500

    refactor redux showcase

--- src/lib/redux/ReduxShowcase.js
+++ src/lib/redux/ReduxShowcase.js
@@ -2,27 +2,21 @@ import React, { Component } from 'react'
 import { Route } from "react-router-dom";
 import { Menu, Header } from 'semantic-ui-react'
 import MarkdownViewer from '../../components/markdownViewer/markdownViewer'
+import TabContainer from '../../components/TabContainer'
 
 import AddSubtract from './AddSubtract'
 
 class ReduxShowcase extends Component {
-    state = { activeItem: '' }
-
-    handleItemClick = (e, { name }) => {
-        this.setState({ activeItem: name });
-        this.props.history.push(`${this.props.match.url}${name}`)
-    }
 
     render() {
-        const { activeItem } = this.state
+        let menuItems = [{ url: '/', name: 'Notes' },
+        { url: '/add-subtract', name: 'AddSubtract' }]
+
         return (
             <div>
                 <Header as='h1'>Redux Router Showcase</Header>
 
-                <Menu tabular>
-                    <Menu.Item name='' active={activeItem === 'Notes'} onClick={this.handleItemClick} >Notes</Menu.Item>
-                    <Menu.Item name='/add-subtract' active={activeItem === 'AddSubtract'} onClick={this.handleItemClick} />
-                </Menu>
+                <TabContainer menuItems={menuItems}></TabContainer>
 
                 <Route path={`${this.props.match.url}/add-subtract`} component={AddSubtract}></Route>
 
