0c900bea606f28a41c24d87981e1019ce677bf5d
Brian Ko <brian.kostar@gmail.com>
2019-01-08 23:23:54 -0500

    add style

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react'
 import MarkdownViewer from '../markdownViewer/markdownViewer'
 
 class Home extends React.Component {
 
     render() {
         return (
-            <div>
+            <Container className='container'>
                 <MarkdownViewer src="https://raw.githubusercontent.com/briankostar/react-resources/master/README.md"></MarkdownViewer>
-            </div>
+            </Container>
         )
     }
 }
